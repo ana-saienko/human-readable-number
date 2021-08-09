6c297d4b90ff01891777835d47899b19321f3db3
saienko-ana <saienko_ana@icloud.com>
2021-08-09 09:38:54 +0200

    init: add the main lines of code (the function does not work)

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function toReadable (number) {
-  
+
+    let numBase = {
+        'one': 1,
+        'two': 2,
+        'three': 3
+    }
+
+    Object.prototype.getKeyByValue = function( value ) {
+        for( var prop in this ) {
+            if( this.hasOwnProperty( prop ) ) {
+                 if( this[ prop ] === value )
+                     return prop;
+            }
+        }
+    }
+
+    return number;
+
 }
